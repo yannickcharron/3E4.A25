63dee6a5a4ac180cd2133b33a2d25566b8af6854
Yannick Charron <ycharron@cstj.qc.ca>
2025-11-26 14:40:22 -0500

    fin validator et fin de la session

--- S11.REST/src/validators/planet.validators.js
+++ S11.REST/src/validators/planet.validators.js
@@ -7,9 +7,15 @@ class PlanetValidators {
     //https://express-validator.github.io/docs/index.html    
     complete() {
         return [
-            , ...this.partial()
+            ...this.partial(),
+            body('name').exists().withMessage('name doit être présent').notEmpty().withMessage('name ne doit pas être vide'),
+            body('position.x').exists().withMessage('position.x doit être présent'),
+            body('position.y').exists().withMessage('position.y doit être présent'),
+            body('position.z').exists().withMessage('position.z doit être présent'),
+            body('discoveredBy').optional().isString().withMessage('discoveredBy doit être une chaîne'),
+            body('discoveryDate').optional().isISO8601().withMessage('discoveryDate doit être une date valide'),
+            body('satellites').optional().isArray().withMessage('satellites doit être un tableau')
         ]
-
     }
 
     partial() {
@@ -20,6 +26,19 @@ class PlanetValidators {
             body('position.z').optional().isFloat({min: -1000, max: 1000}).withMessage('z doit être compris entre -1000 et 1000'),
             body('temperature').optional().isNumeric().withMessage('doit être numérique')
 
+        ]
+    }
+
+    vibeValidator() {
+        return [
+            ...this.partial(),
+            body('name').exists().withMessage('name doit être présent').notEmpty().withMessage('name ne doit pas être vide'),
+            body('position.x').exists().withMessage('position.x doit être présent'),
+            body('position.y').exists().withMessage('position.y doit être présent'),
+            body('position.z').exists().withMessage('position.z doit être présent'),
+            body('discoveredBy').optional().isString().withMessage('discoveredBy doit être une chaîne'),
+            body('discoveryDate').optional().isISO8601().withMessage('discoveryDate doit être une date valide'),
+            body('satellites').optional().isArray().withMessage('satellites doit être un tableau')
         ]
     }
 
